7952ad5aa97953eacaf7b52105a867ee5327a2a4
wrangle1005 <wrangle1005@gmail.com>
2015-11-17 23:47:00 +0800

    finish implementation for matrix invert cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# It is really awesome! I self-learned R before for ML competitions,
+# however, I did not even notice this scoping problem. Thank you for
+# your guidance!
+
+# If not using new operator `<<-`, get() will always return NA since
+# lexical scoping. I really wonder the implementation in details for 
+# this machanism.
+
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Operator `<<-` is usually used in function closure, which prevents
+# the lexical scoping problem.
+makeCacheMatrix <- function(mat = matrix()) {
+    inv <- NULL 
+    set <- function(m) {
+        mat <<- m
+        inv <<- NULL
+    }
+    get <- function() mat
+    setsolve <- function(solve) inv <<- solve
+    getsolve <- function() inv
+    list(set=set, get=get, setsolve=setsolve, getsolve=getsolve)
 }
 
 
 ## Write a short comment describing this function
-
+# Nothing special. Check the value in function before return it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getsolve()
+    if (!is.null(inv)) {
+        message('getting cache data')
+        return(inv)
+    }
+    mat <- x$get()
+    inv <- solve(mat, ...)
+    x$setsolve(inv)
+    inv
 }
